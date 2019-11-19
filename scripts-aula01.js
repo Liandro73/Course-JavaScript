e9a303d37925d7cb7fb727c3184231a23d8ffe36
Dernival_Liandro <liandro.sys@gmail.com>
2019-11-18 23:04:50 -0300

    Commit inicial do Curso de JavaScript completo | Dev Aprender

---
+++ scripts-aula01.js
@@ -0,0 +1,8 @@
+let idade = 28;
+console.log(idade);
+var nome = "Dernival";
+console.log(nome);
+let nomeCompleto = 'Dernival Guimarães Liandro';
+let animação = 'Rick and Morty'
+let notaMedia = 9.8;
+console.log(nomeCompleto, notaMedia, animação);
