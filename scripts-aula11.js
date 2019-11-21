19989b102664d9c27bf9f624e706f098a1ea7e91
Dernival_Liandro <liandro.sys@gmail.com>
2019-11-20 21:58:18 -0300

    Inclusão das aulas 11 e 12, modificação do arquivo scripts.js

---
+++ scripts-aula11.js
@@ -0,0 +1,8 @@
+// Operadores de Atribuição
+let valorTecladoGamer = 100;
+//valorTecladoGamer = valorTecladoGamer + valorTecladoGamer;
+valorTecladoGamer += valorTecladoGamer;
+console.log(valorTecladoGamer);
+
+valorTecladoGamer -= valorTecladoGamer;
+console.log(valorTecladoGamer);
